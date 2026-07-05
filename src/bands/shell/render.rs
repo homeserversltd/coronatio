4415a500233cf71e50ac95a8a71553840e471096
Hermes-01 <hermes-01@home.arpa>
2026-07-05 23:03:04 +0000

    UXPORT-003 stats pane onto UX library

--- src/bands/shell/render.rs
+++ src/bands/shell/render.rs
@@ -29,6 +29,7 @@ const SHELL_UX_CHILDREN: &[&str] = &[
     "library/_toggle.css",
     "library/_visibility-toggle.css",
     "packs/upload.css",
+    "packs/stats.css",
     "shell/document-2-css.css",
 ];
 const SHELL_UX_CONTENTS: &[&str] = &[
@@ -56,6 +57,7 @@ const SHELL_UX_CONTENTS: &[&str] = &[
     include_str!("ux/library/_toggle.css"),
     include_str!("ux/library/_visibility-toggle.css"),
     include_str!("ux/packs/upload.css"),
+    include_str!("ux/packs/stats.css"),
     include_str!("ux/shell/document-2-css.css"),
 ];
 
